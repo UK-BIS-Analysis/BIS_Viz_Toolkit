ad25b1e286bb7efaffe9ac0dee532142d50f12bf
mjs2020 <me@fm.to.it>
2015-07-08 10:52:40 +0100

    Basic library, HTML and CSS skeleton setup.

---
+++ Gruntfile.js
@@ -0,0 +1,153 @@
+// TODO: Sort out build script and put files into build folder
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      indexFile: {
+        src: 'index.dev.html',
+        dest: 'index.html'
+      },
+      modernizr: {
+        src: 'libs/modernizr.custom.js',
+        dest: 'assets/modernizr.custom.js'
+      }
+    },
+
+    useminPrepare: {
+      html: 'index.dev.html',
+      options: {
+        dest: './',
+        flow: {
+          steps: {
+            js: ['concat', 'uglifyjs'],
+            css: ['cssmin']
+          },
+          post: {}
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        rebase: true,
+        relativeTo: './index.html',
+        target: 'assets/libs.min.css'
+      }
+    },
+
+    usemin: {
+      html: 'index.html'
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "scripts",
+          name: "../node_modules/almond/almond",
+          include: ['main'],
+          insertRequire: ['main'],
+          out: 'assets/main.min.js',
+          optimize: "uglify2"
+        }
+      }
+    },
+
+    'regex-replace': {
+      dist: {
+        src: ['index.html'],
+        actions: [
+          {
+            name: 'main',
+            search: '<script data-main="scripts/main" src="libs/requirejs/require.js"></script>',
+            replace:'<script src="assets/main.min.js?build=' + (new Date()).getTime() + '"></script>',
+            flags: 'g'
+          }
+        ]
+      },
+      debug: {
+        src: ['assets/main.min.js'],
+        actions: [
+          {
+            name: 'main',
+            search: 'window.DEBUG=!0',
+            replace: 'window.DEBUG=0',
+            flags: 'g'
+          }
+        ]
+      },
+      modernizr: {
+        src: ['index.html'],
+        actions: [
+          {
+            name: 'main',
+            search:  '<script type="text/javascript" src="libs/modernizr.custom.js"></script>',
+            replace: '<script type="text/javascript" src="assets/modernizr.custom.js"></script>',
+            flags: 'g'
+          }
+        ]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'index.html': 'index.html',     // 'destination': 'source'
+        }
+      }
+    },
+
+    'compress': {
+      main: {
+        options: {
+          archive: 'trademap.zip'
+        },
+        files: [
+          {src: ['data/*'], dest: './', filter: 'isFile'}, // includes files in path
+          {src: ['img/**', 'pages/**', 'assets/**', 'libs/**', 'styles/**', 'scripts/**'], dest: './'}, // includes files in path and its subdirs
+          {src: ['./*.txt', './*.html', './*.md', './*.ico', '!./passwords.md'], dest: './', filter: 'isFile'}
+        ]
+      }
+    }
+
+
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-filerev');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-regex-replace');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  // Default task(s).
+  grunt.registerTask('default', [
+    'copy:indexFile',
+    'copy:modernizr',
+    'useminPrepare',
+    'concat:generated',
+    'cssmin:generated',
+    'uglify:generated',
+    'usemin',
+    'requirejs:compile',
+    'regex-replace:debug',
+    'regex-replace:dist',
+    'regex-replace:modernizr',
+    'htmlmin:dist',
+    'compress'
+  ]);
+
+};
---
+++ js/helpers/gui.js
@@ -0,0 +1,30 @@
+/*jslint browser: true*/
+/*jslint white: true */
+/*jslint vars: true */
+/*global $, Modernizr, d3, dc, crossfilter, document, console, alert, define, DEBUG, queryObject, btoa */
+
+define([], function() {
+  'use strict';
+
+  var gui = {
+
+    setup: function () {
+      // ADD SPECIFIC MENU BEHAVIOURS
+      // Build links with http://www.sharelinkgenerator.com if needed
+      $('#facebookShareLink').on('click', function (e) {
+        e.preventDefault();
+        var winTop = (screen.height / 2) - (520 / 2);
+        var winLeft = (screen.width / 2) - (350 / 2);
+        window.open('https://www.facebook.com/sharer/sharer.php?u='+window.location.href, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + 520 + ',height=' + 350);
+      });
+      $('#tweetLink').on('click', function (e) {
+        e.preventDefault();
+        var winTop = (screen.height / 2) - (520 / 2);
+        var winLeft = (screen.width / 2) - (350 / 2);
+        window.open('https://twitter.com/share?url='+window.location.href, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + 520 + ',height=' + 350);
+      });
+    }
+
+  };
+  return gui;
+});
